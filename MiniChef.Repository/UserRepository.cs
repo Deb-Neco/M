9dfa0ac7a467fbf9eab7b733cb91608d4c1b0263
Deborah Silva <deborah.silva@scania.com>
2023-09-14 09:14:43 -0300

    Validação de entrada

--- MiniChef.Repository/UserRepository.cs
+++ MiniChef.Repository/UserRepository.cs
@@ -18,8 +18,20 @@ namespace MiniChef.Repository
         {
             _connectionString = connectionString;
         }
+
+
         public async Task<User> CreateUserRepository(User user)
         {
+            // Validação de entrada
+            if (string.IsNullOrWhiteSpace(user.nome))
+            {
+                throw new ArgumentException("O campo 'nome' é obrigatório.");
+            }
+
+            if (string.IsNullOrWhiteSpace(user.senha) || user.senha.Length < 6)
+            {
+                throw new ArgumentException("A senha deve ter pelo menos 6 caracteres.");
+            }
 
             using (var connection = new NpgsqlConnection(_connectionString))
             {
@@ -33,6 +45,22 @@ namespace MiniChef.Repository
 
             return user;
         }
+
+        //public async Task<User> CreateUserRepository(User user)
+        //{
+
+        //    using (var connection = new NpgsqlConnection(_connectionString))
+        //    {
+        //        await connection.OpenAsync();
+
+        //        var sql = "INSERT INTO Users (nome, senha) VALUES (@Nome, @Senha) RETURNING id";
+        //        var userId = await connection.ExecuteScalarAsync<int>(sql, user);
+
+        //        user.id = userId;
+        //    }
+
+        //    return user;
+        //}
 
     }
 }
